eb8ead669a5245465018c6cb9c63e1779fa1758b
Maksym Stupakov <maksym.stupakov@gmail.com>
2017-08-27 13:04:40 +0300

    Added support of returning shared pointer.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
+#include <cstring>
 #include <iostream>
 #include <vector>
 #include <string>
 #include <memory>
+#include <functional>
 
 template<typename T>
 class ObjPool {
@@ -52,6 +54,14 @@ class ObjPool {
       return ObjUPtr(get(std::forward<Args> (args)...),
           [this] (T* obj) { this->free(obj); } );
     }
+
+    using ObjSPtr = std::shared_ptr<T>;
+
+    template<typename... Args>
+    ObjSPtr get_shared(Args&&... args) {
+      return ObjSPtr(get(std::forward<Args> (args)...),
+          [this] (T* obj) { this->free(obj); } );
+    }
 #endif
 
     void free(T* obj) {
@@ -86,7 +96,7 @@ struct A {
     std::cout << __PRETTY_FUNCTION__ << " This: " << this << std::endl;
   }
 
-  A& operator=(const A&) {
+  void operator=(const A&) {
     std::cout << __PRETTY_FUNCTION__ << " This: " << this << std::endl;
   }
 };
@@ -107,7 +117,7 @@ struct B {
     std::cout << __PRETTY_FUNCTION__ << " This: " << this << std::endl;
   }
 
-  B& operator=(const B&) {
+  void operator=(const B&) {
     std::cout << __PRETTY_FUNCTION__ << " This: " << this << std::endl;
   }
 
@@ -115,6 +125,14 @@ struct B {
     std::cout << __PRETTY_FUNCTION__ << " This: " << this << std::endl;
   }
 };
+
+struct L2Frame {
+  char payload[1500];
+};
+
+void test_shared(ObjPool<B>::ObjSPtr ptr) {
+  ptr->method();
+}
 
 int main() {
   ObjPool<A> pool(5);
@@ -156,10 +174,27 @@ int main() {
   ObjPool<B>::ObjUPtr u_ptr1 = pool_b.get_unique(b2);
   ObjPool<B>::ObjUPtr u_ptr2 = pool_b.get_unique(b2);
   ObjPool<B>::ObjUPtr u_ptr3 = pool_b.get_unique(2, '%', "World!");
+  ObjPool<B>::ObjSPtr s_ptr4 = pool_b.get_shared(3, '&', "World!");
 
   u_ptr1->method();
   u_ptr2->method();
   u_ptr3->method();
+  s_ptr4->method();
+
+  test_shared(s_ptr4);
+
+  ObjPool<L2Frame> pool_f(8);
+  ObjPool<L2Frame>::ObjUPtr f_ptr1 = pool_f.get_unique();
+  ObjPool<L2Frame>::ObjUPtr f_ptr2 = pool_f.get_unique();
+  ObjPool<L2Frame>::ObjUPtr f_ptr3 = pool_f.get_unique();
+  ObjPool<L2Frame>::ObjUPtr f_ptr4 = pool_f.get_unique();
+
+  L2Frame* f_ptr6 = pool_f.get();
+  pool_f.free(f_ptr6);
+
+  std::cout << "Size: " << sizeof(*f_ptr1) << std::endl;
+
+  memset(f_ptr1->payload, 0, sizeof(*f_ptr1));
 
   return 0;
 }
